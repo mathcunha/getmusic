fa6ca7cf780707ab28f04b8cddc70b5f5be41211
Matheus Cunha <mathcunha@gmail.com>
2015-02-21 22:58:14 +0000

    searching for Album if music not found

--- gmusic/1337.go
+++ gmusic/1337.go
@@ -16,30 +16,47 @@ func GetMagnetLinks(m []Music) []string {
 }
 
 func GetMagnetLink(m Music) string {
-	values := url.Values{}
-	values.Set("", fmt.Sprint(m.Artist, " ", m.Name, " ", m.Year))
-	query := values.Encode()
-	query = query[1:len(query)]
+	for i := 0; i < 2; i++ {
 
-	//Searching the music
-	url := fmt.Sprint("http://1337x.to/search/", query, "/1/")
-	body := GetUrl(url)
-	href := LeetContent(body)
+		values := url.Values{}
+		if i == 0 {
+			values.Set("", fmt.Sprint(m.Artist, " ", m.Name, " ", m.Year))
+		} else {
+			values.Set("", fmt.Sprint(m.Artist, " ", m.Album, " ", m.Year))
+		}
+		//fmt.Println(values)
+		query := values.Encode()
+		query = query[1:len(query)]
 
-	//Searching the magnet link
-	url = fmt.Sprint("http://1337x.to", href)
-	body = GetUrl(url)
-	href = LeetMagnet(body)
+		//Searching the music
+		url := fmt.Sprint("http://1337x.to/search/", query, "/1/")
+		body := GetUrl(url)
+		href := LeetContent(body)
 
-	return href
+		//Searching the magnet link
+		url = fmt.Sprint("http://1337x.to", href)
+		body = GetUrl(url)
+		href = LeetMagnet(body)
+
+		if i == 0 {
+			if len(href) > 0 {
+				//return by music name
+				return href
+			}
+		} else {
+			//return by album name
+			return href
+		}
+	}
+	return ""
 }
 
 func LeetMagnet(b []byte) string {
-	pattern := regexp.MustCompile("<ul class=\"download-links\">(?P<data>(.|\n)+)<ul class=\"category-name\">")
+	pattern := regexp.MustCompile("<ul class=\"download-links\">(?P<data>(.|\n)+)Magnet Download")
 	mp := map[string]string{}
 	FindPattern(pattern, b, &mp)
 
-	//fmt.Printf("content [%v]", mp["data"])
+	//fmt.Printf("torrent page content [%v]", mp["data"])
 
 	pattern = regexp.MustCompile("<a href=\"(?P<href>.*)\" oncli")
 	FindPattern(pattern, []byte(mp["data"]), &mp)
@@ -56,6 +73,8 @@ func LeetContent(b []byte) string {
 
 	pattern = regexp.MustCompile("<strong><a href=\"(?P<href>.*)\"")
 	FindPattern(pattern, []byte(mp["data"]), &mp)
+
+	//fmt.Printf("link [%v]", mp["href"])
 
 	return mp["href"]
 }
