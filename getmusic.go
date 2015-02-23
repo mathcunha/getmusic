ccbd881502aa333743e5014c84f0b5b838f32b98
Matheus Cunha <mathcunha@gmail.com>
2015-02-23 18:47:50 +0000

    enabling web app

--- getmusic.go
+++ getmusic.go
@@ -1,30 +1,14 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/mathcunha/getmusic/gmusic"
+	"net/http"
 )
 
-var playlist string
-
-func init() {
-	flag.StringVar(&playlist, "playlist", "http://www.deezer.com/playlist/1127687863", "Deezer playlist url")
+func main() {
+	http.HandleFunc("/view/", staticHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
-func main() {
-
-	flag.Parse()
-
-	s := gmusic.Deezer{}
-
-	fmt.Println("Loading the playlist")
-	m, _ := gmusic.GetPlaylist(playlist, &s)
-
-	fmt.Println("Searching the magnet links")
-	magnet := gmusic.GetMagnetLinks(m)
-
-	for i, v := range m {
-		fmt.Printf("%v - [%v] \n", v, magnet[i])
-	}
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
 }
