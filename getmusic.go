1b7241e904260e424e6a651508aa1cdf33eb0ac9
Matheus Cunha <mathcunha@gmail.com>
2015-02-24 00:43:56 +0000

    preparing to Heroku

--- getmusic.go
+++ getmusic.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"github.com/mathcunha/getmusic/gmusic"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/view/", staticHandler)
 	http.HandleFunc("/api/", gmusic.RestHandlerV1)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(getPort(), nil)
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func getPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "8080"
+		log.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
